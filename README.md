f6abc31f30ef62a4556c7b1def9fd195e97a3f96
anthonytorresramos@gmail.com <anthonytorresramos@gmail.com>
2021-10-20 18:39:49 +0800

    version 1.00

--- README.md
+++ README.md
@@ -2,3 +2,43 @@
 
 <h1>Setup</h1>
 <p>Run npm install bootstrap</p>
+<hr>
+<h1>Versions</h1>
+<p>1.00</p>
+<ol>
+   <li>
+      assets
+      <ol>
+         <li>
+            css
+            <ol>
+               <li>style.css</li>
+            </ol>
+         </li>
+         <li>fileuploads</li>
+         <li>images</li>
+         <li>
+            js
+            <ol>
+               <li>app.js</li>
+            </ol>
+         </li>
+         <li>multimedia</li>
+      </ol>
+   </li>
+   <li>autoload.php</li>
+   <li>
+      db
+      <ol>
+         <li>conf.php</li>
+      </ol>
+   </li>
+   <li>index.php</li>
+   <li>
+      php
+      <ol>
+         <li>functions.php</li>
+      </ol>
+   </li>
+</ol>
+<ol><li>Added Boostrap</li></ol>
--- php/functions.php
+++ php/functions.php
@@ -33,3 +33,41 @@ function allPath()
 function defaultTest(){
     return '<h3>Functions from php/functions.php!';
 }
+
+function listFolderFiles($dir){
+    $exclude = array(
+        'db',
+    );
+    $ffs = scandir($dir);
+
+    unset($ffs[array_search('.', $ffs, true)]);
+    unset($ffs[array_search('..', $ffs, true)]);
+    unset($ffs[array_search('.git', $ffs, true)]);
+    unset($ffs[array_search('node_modules', $ffs, true)]);
+    unset($ffs[array_search('test', $ffs, true)]);
+    unset($ffs[array_search('package-lock.json', $ffs, true)]);
+    unset($ffs[array_search('package.json', $ffs, true)]);
+    unset($ffs[array_search('README.md', $ffs, true)]);
+
+    // prevent empty ordered elements
+    if (count($ffs) < 1)
+        return;
+        
+        
+    echo '<ol>';
+    foreach($ffs as $key => $ff){
+        
+        
+        echo '<li>'.$ff;
+        if(is_dir($dir.'/'.$ff)) listFolderFiles($dir.'/'.$ff);
+       
+        
+        echo '</li>';
+    }
+    echo '</ol>';
+    
+
+}
+
+
+listFolderFiles('/xampp2022/htdocs/corephp-templates');
